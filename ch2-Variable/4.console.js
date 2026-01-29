f8ca9afc3a355d786308a88300d34e954104e6c4
MohinKhan <mohinpathan2004@gmail.com>
2026-01-29 10:38:45 +0530

    Add files via upload

---
+++ ch2-Variable/4.console.js
@@ -0,0 +1,6 @@
+console.log("Good Morning")
+
+a=10
+b=20
+
+console.log(a+b)
---
+++ ch2-Variable/5.comment.js
@@ -0,0 +1,12 @@
+
+
+// two types comment in javascript
+
+
+// This is sinle line Comment
+
+ /*this is multiple line Comment
+ multiple line means it is used 
+ for taking more than one.*/
+
+
