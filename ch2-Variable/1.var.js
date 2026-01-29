b8749c34fe55b3602fc195d820b4b28527396f99
MohinKhan <mohinpathan2004@gmail.com>
2026-01-29 10:38:23 +0530

    Add files via upload

---
+++ ch2-Variable/1.var.js
@@ -0,0 +1,16 @@
+var greet = "good morning"
+
+function Name(){
+    var name="Mohin"
+    console.log(name)
+}
+
+console.log(greet)
+
+greet="good evening"
+console.log(greet)
+
+var greet="good night"
+console.log(greet)
+
+Name();
