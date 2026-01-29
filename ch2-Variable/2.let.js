43e1b8493b448562284c6222633659aea04b023c
MohinKhan <mohinpathan2004@gmail.com>
2026-01-29 10:36:42 +0530

    Add files via upload

---
+++ ch2-Variable/2.let.js
@@ -0,0 +1,24 @@
+let greet="Hello";
+
+function Name(){
+
+    let Name="Mohin"
+    console.log(Name);
+
+    if(true){
+        let Name="Pathan"
+        console.log(Name)
+    }
+}
+
+console.log(greet)
+Name();
+
+// Re-intilazation is possible
+
+let Age=" your age"
+Age="My age is 20"
+console.log(Age)
+
+// Re-Declare is not possible in let
+// let Age=
---
+++ ch2-Variable/3.const.js
@@ -0,0 +1,22 @@
+
+// const must be intilazation one time like this...const A=...!
+//  but you can use differnt varible.
+
+const A= 20;
+
+const B=10;
+
+console.log(A*B)
+
+
+// variable cannot be re declared
+// const A=
+
+
+const Name="Mohin"
+
+const Name2={Name:"Pathan",age:"20"}
+
+Name2.Name="Mohin"
+
+console.log(Name2)
