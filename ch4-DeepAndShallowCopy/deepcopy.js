66161098a7d168d75a466ff33ee5d6c83e92cd2b
MohinKhan <mohinpathan2004@gmail.com>
2026-01-30 10:25:43 +0530

    Add files via upload

---
+++ ch4-DeepAndShallowCopy/deepcopy.js
@@ -0,0 +1,14 @@
+
+let student1={Name:"Mohin",Age:'20'}
+
+console.log("student1",student1)
+
+let student2=student1;
+console.log("student2",student2)
+
+student2.Name="Mohin Pathan"
+
+console.log("After change student2",student2)
+console.log("after change studnet1",student1)
+
+console.log(" ")
---
+++ ch4-DeepAndShallowCopy/shallowcopy.js
@@ -0,0 +1,8 @@
+
+let a=10;
+let b=a;
+
+b=20;
+
+console.log("a=",a)
+console.log("b=",b)
