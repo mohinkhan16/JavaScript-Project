b6d2c6c9fe9c643c70ca325f286f55f9b80e1929
MohinKhan <mohinpathan2004@gmail.com>
2026-02-03 10:33:35 +0530

    Add files via upload

---
+++ ch6-TypeConverstion/index.js
@@ -0,0 +1,247 @@
+// There are two type of Type covrestion
+// 1:-Implicit
+console.log("Implicit Example");
+console.log("7" - 4); 
+
+// 2:-Explicit
+console.log("Explicit Example");
+console.log("5" +3);
+
+// This is mixed example of type converstions
+// Example:-1
+console.log("Example 1:");
+console.log("4"*3);
+
+
+// Example:-2
+console.log("Example 2:");
+console.log("20"/5);
+
+
+// Example:-3
+console.log("Example 3:");
+console.log(Number("21") + 9);
+
+// Example:-4
+console.log("Example 4:");
+console.log(+"abc");
+
+
+// Example:-5
+console.log("Example 5:");
+console.log(+"30"+10);
+
+
+// Example:-6
+console.log("Example 6:");
+console.log("10"%5);
+
+
+// Example:-7
+console.log("Example 7:");
+console.log(parseInt("50px"));
+
+
+// Example:-8
+console.log("Example 8:");
+console.log(parseFloat("20.55"));
+
+
+// Example:-9
+console.log("Example 9:");
+console.log("20"+"abc");
+
+
+// Example:-10
+console.log("Example 10:");
+console.log("5"+3);
+
+
+// Example:-11
+console.log("Example 11:");
+console.log(parseInt(56.32));
+
+
+// Example:-12
+console.log("Example 12:");
+console.log(parseFloat(74.23));
+
+
+// Example:-13
+console.log("Example 13:");
+console.log(String(99)+1);
+
+
+// Example:-14
+console.log("Example 14:");
+console.log(String(true));
+
+
+// Example:-15
+console.log("Example 15:");
+console.log("true"+5);
+
+
+// Example:-16
+console.log("Example 16:");
+console.log(true+2);
+
+
+// Example:-17
+console.log("Example 17:");
+console.log(false-1);
+
+
+// Example:-18
+console.log("Example 18:");
+console.log(true+true);
+
+// Example:-19
+console.log("Example 19:");
+console.log(Number(0));
+
+
+// Example:-20
+console.log("Example 20:");
+console.log(Boolean(""));
+
+
+// Example:-21
+console.log("Example 21:");
+console.log(Boolean(5));
+
+
+// Example:-22
+console.log("Example 22:");
+console.log(Boolean("abc"));
+
+
+// Example:-23
+console.log("Example 23:");
+console.log(null+5);
+
+
+// Example:-24
+console.log("Example 24:");
+console.log(String(null));
+
+
+// Example:-25
+console.log("Example 25:");
+console.log(undefined + 5);
+
+
+// Example:-26
+console.log("Example 26:");
+console.log(null *5);
+
+
+// Example:-27
+console.log("Example 27:");
+console.log(undefined-2);
+
+
+// Example:-28
+console.log("Example 28:");
+console.log(Boolean(undefined));
+
+
+// Example:-29
+console.log("Example 29:");
+console.log(Number("word"));
+
+
+// Example:-30
+console.log("Example 30:");
+console.log(true +"6");
+
+
+// Example:-31
+console.log("Example 31:");
+console.log(0+false);
+
+
+// Example:-32
+console.log("Example 32:");
+console.log("1"+true);
+
+
+// Example:-33
+console.log("Example 33:");
+console.log("1"-false);
+
+
+// Example:-34
+console.log("Example 34:");
+console.log("8"*"2"+"1");
+
+
+// Example:-35
+console.log("Example 35:");
+console.log(parseInt("85year"));
+
+
+// Example:-36
+console.log("Example 36:");
+console.log(parseFloat("45.67kg"));
+
+
+// Example:-37
+console.log("Example 37:");
+console.log(7-true);
+
+
+// Example:-38
+console.log("Example 38:");
+console.log(9-false);
+
+// Example:-39
+console.log("Example 39:");
+console.log(Boolean("false"));
+
+
+// Example:-40
+console.log("Example 40:");
+console.log(NaN+10);
+
+// Example:-41
+console.log("Example 41:");
+console.log(" "-8);
+
+// Example:-42
+console.log("Example 42:");
+console.log(Number('')+6);
+
+// Example:-43
+console.log("Example 43:");
+console.log(true+"6");
+
+// Example:-44
+console.log("Example 44:");
+console.log( true+true);
+
+// Example:-45
+console.log("Example 45:");
+console.log(false-true);
+
+
+// Example:-46
+console.log("Example 46:");
+console.log(Boolean("num"));
+
+// Example:-47
+console.log("Example 47:");
+console.log(String("hello"));
+
+// Example:-48
+console.log("Example 48:");
+console.log("Hello"-2);
+
+// Example:-49
+console.log("Example 49:");
+console.log("hello"+2);
+
+
+// Example:-50
+console.log("Example 50:");
+console.log("8"-2);
---
+++ ch7-ControlFlow/if-else.js
@@ -0,0 +1,15 @@
+// IF
+
+const age=true;
+if(age){
+    console.log("you can give a vote");
+}
+
+
+// IF-ELSE
+
+const Age=18;
+
+if(age<=18){
+    console.log("you can drive a bike")
+}else("you can't drive a bike")
