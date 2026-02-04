7b0d0f27354b566bff7f079fc257c985fc1686c5
MohinKhan <mohinpathan2004@gmail.com>
2026-02-04 15:18:14 +0530

    Add files via upload

---
+++ ch8-Break_Continue/Break.js
@@ -0,0 +1,22 @@
+// Example no:-1
+
+let num=[1,2,3,4,5,6,7,8,9,10];
+
+for(let i=0;i<num.length;i++){
+    if(num[i]===7){
+        break;
+    }
+    console.log(i);
+}
+
+
+// Example no:-2
+
+let colors=["red","green","blue","yellow","purple"];
+
+for(let i=0;i<colors.length;i++){
+    if(colors[i]==="blue"){
+        break;
+    }
+    console.log(colors[i]);
+}
---
+++ ch8-Break_Continue/Continue.js
@@ -0,0 +1,25 @@
+
+// This is continue statement example
+
+let oddnumber=20;
+
+for(let i=1;i<=oddnumber;i++){
+    if(i%2===0){
+        continue;
+    }
+    console.log(i);
+}
+
+
+// example no:-2
+
+console.log("--------");
+
+let Evennumber=20;
+
+for(let i=1;i<=Evennumber;i++){
+    if(i%2!==0){
+        continue;
+    }
+    console.log(i);
+}
