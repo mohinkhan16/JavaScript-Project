8a7458596b65ebf704ccd839626cd41ee921e904
MohinKhan <mohinpathan2004@gmail.com>
2026-02-05 15:34:04 +0530

    Add files via upload

---
+++ ch9-Gread/From/From2/Form.js
@@ -0,0 +1,17 @@
+document.getElementById("form").addEventListener("submit",(e)=>{
+    e.preventDefault();
+
+    let age=document.getElementById("age").value;
+
+    let result=document.getElementById("result");
+
+    if(age>=60){
+        result.innerHTML=`<h1>you are senior Citizen</h1>`;
+    }else if(age>=30){
+        result.innerHTML =`<h1>you are adult</h1>`;
+    }else if(age>=18){
+        result.innerHTML=`<h1>you are young adult</h1>`;
+    }else{
+        result.innerHTML=`<h1> you are child</h1>`;
+    }
+})
