1724b3f47ca72f368b8c1a04e848957c4493d310
MohinKhan <mohinpathan2004@gmail.com>
2026-02-10 14:33:45 +0530

    Add files via upload

---
+++ ch10-Funcation/1-funcation.js
@@ -0,0 +1,21 @@
+
+// funcation declaration
+
+function color(){
+
+    console.log("this is red color");
+}
+
+// funcation invocation
+
+color();
+
+function multiply(a,b){
+
+    return a*b;
+}
+
+console.log(multiply(5,6));
+console.log(multiply(6,6));
+console.log(multiply(7,6));
+console.log(multiply(1,4));
---
+++ ch10-Funcation/2-funcation-arjument.js
@@ -0,0 +1,22 @@
+
+
+// =====function declaration===
+
+// function division(a,b){
+
+//     console.log(a/b);;
+// }
+
+// ====passing arguments to the function==
+// division(10,5);
+// division(20,5);
+// division(30,5);
+
+
+function addition(a=5,b=10){
+    console.log(a+b);
+}
+
+addition();
+addition(20,30);
+
