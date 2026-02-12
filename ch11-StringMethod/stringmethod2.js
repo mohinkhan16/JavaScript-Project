2471db857c9785c39070ba988a6771697b2d0846
MohinKhan <mohinpathan2004@gmail.com>
2026-02-12 14:35:39 +0530

    Update stringmethod2.js

--- ch11-StringMethod/stringmethod2.js
+++ ch11-StringMethod/stringmethod2.js
@@ -1,92 +1,92 @@
-// 1:-  metchmethod
-  //Metchmethod is use for give us to full details and
+// 1:-  Match method
+  //Match method is use for give us to full details and
   //   //  also give index if is it no working then he give us to Null output.
 
 let play ="i am playing cricket";
-console.log(play.match("am"));
+console.log("Match Method:-",play.match("am"));
 
 // 2:-repeat
 //make a new string and copy strings
 
 const Name="Mohin";
-console.log(Name.repeat(2));
+console.log("Repeat:-",Name.repeat(2));
 
 // 3:-replace
-// first match are replce
-// searching a new string a pattern and retrun a new string
+// first match are replace
+// searching a new string a pattern and return a new string
 
 let word="this is book";
-console.log(word.replace("b","B"));
+console.log("replace:-",word.replace("b","B"));
 
-// 4:-replaceall
-// replaceall work for replace all same latter 
+// 4:-replace all
+// replace all work for replace all same latter 
  word="this is book";
- console.log(word.replaceAll("o","O"));
+ console.log("Replace All:-",word.replaceAll("o","O"));
 
  //5:-search
-//  search a value and retrun a index position
-// search has a inculd a regular  expression 
+//  search a value and return a index position
+// search has a include a regular  expression 
 // index has a not a include a regular expression that the different between this two 
 
 let book="War and Peace";
-console.log(book.search("a"));
+console.log("Search:-",book.search("a"));
 
 // 6:-slice 
-//abstract a part of string and retrun a new string
+//abstract a part of string and return a new string
 
 let Animal="This is so many animal";
-console.log(Animal.slice(2));
-console.log(Animal.slice(6));
-console.log(Animal.slice(-1));
+console.log("Slice:-",Animal.slice(2));
+console.log("Slice:-",Animal.slice(6));
+console.log("Slice:-",Animal.slice(-1));
 
 // 7:-split 
 //firstly they convert string into array
 
 let mobile="This is my new mobile";
-console.log(mobile.split(" "))   //with space
-console.log(mobile.split(""))   //without space
-console.log(mobile.split("i"))   //removing latter
+console.log("Split:-",mobile.split(" "))   //with space
+console.log("Split:-",mobile.split(""))   //without space
+console.log("Split:-",mobile.split("i"))   //removing latter
 
-// 8:-startwith
-// give us to output in booleen value 
+// 8:-start with
+// give us to output in boolean value 
 // if is latter in the string then output is true rather then output is false
 
 let text="hello everyone";
 
-console.log(text.startsWith("h"));
+console.log("Start With:-",text.startsWith("h"));
 
 // 9:-substring
-//substring does not chane orignal array
+//substring does not change original array
 //
 
 let number="abcdefg";
-console.log(number.substring(2,5));
-console.log(number.substring(5,2));
-console.log(number.substring(-1));//give full text
+console.log("Substring:-",number.substring(2,5));
+console.log("Substring:-",number.substring(5,2));
+console.log("Substring:-",number.substring(-1));//give full text
 
-// 10:-uppercash
-//write touppercash()
-//if text has a lowercash we can change into uppercash
+// 10:-uppercase
+//write touppercase()
+//if text has a lowercase we can change into uppercase
 
 let upper="good morning";
-console.log(upper.toUpperCase());
+console.log("Uppercase:-",upper.toUpperCase());
 
-// 11:-lowercash
-//write tolowercash()
-//if text has a uppercash wa can change into lowercash
+// 11:-lowercase
+//write tolowercase()
+//if text has a uppercase wa can change into lowercase
 
 let lower="GOOD AFTERNOON";
-console.log(lower.toLowerCase());
+console.log("Lowercase:-",lower.toLowerCase());
 
 // 12:-trim 
 // use for remove white space
 
 let remove="  hello  everyone   ";
 
-console.log(remove.length);
+console.log("Trim:-",remove.length);
 
-console.log(remove.trim().length);
+console.log("Trim:-",remove.trim().length);
 
-console.log(remove.trimStart().length);
+console.log("Trim:-",remove.trimStart().length);
 
-console.log(remove.trimEnd().length);
+console.log("Trim:-",remove.trimEnd().length);
