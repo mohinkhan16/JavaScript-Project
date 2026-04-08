1d4ed6ee8f8d63a06762ad63bd9fe48e64ebe762
MohinKhan <mohinpathan2004@gmail.com>
2026-04-08 15:33:37 +0530

    Add files via upload

---
+++ ArrayMethod-Revesion/1.array.js
@@ -0,0 +1,35 @@
+
+
+//this is array methods and use length to total data..
+
+let data =["ishan",20,"Mumbai"]
+
+//first index print
+console.log(data[0]);
+
+console.log(data);
+console.log(data.length);
+console.log(typeof data);
+
+
+//First and Last elemnet chaning...
+
+let name=["Amir","Sumit","ishan"];
+
+name[0]="Ravi"
+
+console.log("After name changing..",name);
+
+//Last element checking.../last index print
+
+var lastelemnet = name[name.length-1];
+console.log("last element",lastelemnet)
+
+//modification 
+
+let document=["pancard","aadharcard","LC","Photos"];
+
+document[1]="Passport";
+
+console.log(document);
+ 
